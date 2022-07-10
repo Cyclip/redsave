86baf646ccad785c6b64122a1db8808dbc09d8ae
Ifaz Ahmed <gdneop@gmail.com>
2022-07-10 14:11:58 +0100

    Add infinite scrolling

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
+// when page loads (only on reddit)
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-        console.log("Complete", tab, tabId);
         chrome.scripting.executeScript({
             target: {tabId: tabId},
             func: addButtons,
@@ -10,12 +10,7 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension"
-        );
-        
-        if (request.request === "downloadUrl") {
+        if (request.action === "downloadUrl") {
             let [result, response] = download(request.url);
 
             sendResponse({
@@ -28,12 +23,13 @@ chrome.runtime.onMessage.addListener(
 
 function addButtons() {
     // get all current posts
-    let posts = document.getElementsByClassName("_1oQyIsiPHYt6nx7VOmd1sz");
+    let posts = document.querySelectorAll("._1oQyIsiPHYt6nx7VOmd1sz:not(.redsaveButton");
     
     // add download button to all posts
     for (let i = 0; i < posts.length; i++) {
         // url of post
         let post = posts[i];
+        post.classList.add("redsaveButton");
 
         // check if url can be quickly retrieved instead of 2 API calls
         let url = tryQuickUrl(post);
@@ -42,15 +38,15 @@ function addButtons() {
         button.innerText = "Download";
         button.onclick = function() {
             chrome.runtime.sendMessage({
-                request: "downloadUrl",
+                action: "downloadUrl",
                 url: url,
             }, function(response) {
-                console.log("WEEE", response);
+                console.log("Downloaded response:", response);
               });
-              
         };
 
         posts[i].lastChild.lastChild.lastChild.appendChild(button);
+        console.log(`Created button with url ${url} at`, button);
     }
 }
 
---
+++ contentScript.js
@@ -0,0 +1,65 @@
+// Content script to be injected into reddit pages
+
+const SCROLL_THRESHOLD = 5000
+
+function tryQuickUrl(post) {
+    // check for imgs
+    let imgs = post.querySelectorAll("img");
+    for (let i = 0; i < imgs.length; i++) {
+        let url = new URL(imgs[i].src);
+
+        if (url.href.includes("award_images")) {
+            continue;
+        }
+
+        if (url.hostname === "preview.redd.it") {
+            // preview image, transform into final image
+            return "https://i.redd.it" + url.pathname + url.search
+        } else if (url.hostname === "external-preview.redd.it") {
+            return url.href;
+        }
+    }
+
+    // unknown
+    return post.lastChild.children[1].querySelector("a").href;
+}
+
+function addButtons() {
+    // get all current posts
+    let posts = document.querySelectorAll("._1oQyIsiPHYt6nx7VOmd1sz:not(.redsaveButton");
+    
+    // add download button to all posts
+    for (let i = 0; i < posts.length; i++) {
+        // url of post
+        let post = posts[i];
+        post.classList.add("redsaveButton");
+
+        // check if url can be quickly retrieved instead of 2 API calls
+        let url = tryQuickUrl(post);
+
+        let button = document.createElement("button");
+        button.innerText = "Download";
+        button.onclick = function() {
+            chrome.runtime.sendMessage({
+                action: "downloadUrl",
+                url: url,
+            }, function(response) {
+                console.log("Downloaded response:", response);
+              });
+        };
+
+        posts[i].lastChild.lastChild.lastChild.appendChild(button);
+        console.log(`Created button with url ${url} at`, button);
+    }
+}
+
+var observables = document.querySelector(".rpBJOHq2PR60pnwJlUyP0");
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        addButtons();
+    });    
+});
+
+var config = {childList: true};
+observer.observe(observables, config);
